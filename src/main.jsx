9493741c12f8e9ce9fd0604c6fe25244790c37f8
sofi-andrade <sofiaisabelleandraderocha@gmail.com>
2026-09-20 18:31:54 -0300

    Alterações feitas na cor de fundo

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter , Route , Routes} from "react-router-dom";
 
-
+import Login from './pages/Login/Login';
 import Home  from './pages/Home/Home'
 import Local  from './pages/Local/Local'
-import Noticia from "./pages/Noticia/Noticia"
+import Noticia from './pages/Noticia/Noticia'
+
 
 
 
@@ -18,6 +19,8 @@ createRoot(document.getElementById('root')).render(
       <Route path='/' element={<Home/>}/>
       <Route path='/Local' element={<Local/>}/>
       <Route path='/Noticia' element={<Noticia/>}/>
+      <Route path='/Login' element={<Login/>}/>
+
     </Routes>
   
   </BrowserRouter>,
