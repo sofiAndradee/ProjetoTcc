2ef7d10bd806d63e0137821d064aca9cc33c04eb
sofi-andrade <sofiaisabelleandraderocha@gmail.com>
2026-09-24 19:47:04 -0300

    Feito o merge entre a Branch teste

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+// Importação correta dos dois controladores limpos do Pool MySQL
+const userController = require("../../controller/userController");
+const projetoController = require("../../controller/projetoController");
+
+// =========================================================================
+// 👥 ROTAS DE USUÁRIOS E LOGIN (Vêm do userController)
+// =========================================================================
+router.get("/users", userController.ListarUsuarios);
+router.post("/users", userController.adicionarUsuario);
+router.post("/login", userController.efetuarLogin);
+
+// =========================================================================
+// 🏢 ROTAS DE PROJETOS E SOLICITAÇÕES (Vêm do projetoController)
+// =========================================================================
+// --- ROTAS DE PROJETOS ---
+router.get("/projetos", projetoController.listarProjetos);
+router.post("/projetos", projetoController.cadastrarProjeto);
+router.delete("/projetos/:id", projetoController.deletarProjeto);
+router.put("/projetos/:id/decisao", projetoController.decidirProposta); // <-- GARANTA QUE ESSA LINHA ESTÁ EXATAMENTE ASSIM!
+
+module.exports = router;
+
+
